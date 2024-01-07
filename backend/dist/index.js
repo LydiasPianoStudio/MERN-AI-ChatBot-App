fc5eba196d01c3114291706b7607de96ffe8a9be
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2024-01-07 16:25:55 -0500

    Integrate MongoDB
    Tested and connected database

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 //connecions and listeners
+const PORT = process.env.PORT || 5000;
 connectToDatabase().then(() => {
-    app.listen(5000, () => console.log("Server Open Connected & to Database! Tee HEE HEE"));
+    app.listen(PORT, () => console.log("Server Open & Connected to Database! TEE HEE HEE"));
 })
     .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
